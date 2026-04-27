3c56af8594ebedba59a8dd03b8c969786bfff7ca
Your Name <your.email@example.com>
2026-04-27 23:13:49 +0500

    cleanup: remove scratch files

--- scratch/debug-mongo.js
+++
@@ -1,4 +0,0 @@
-const { MongoStore } = require('connect-mongo');
-console.log('Type of destructured MongoStore:', typeof MongoStore);
-console.log('Type of MongoStore.create:', typeof MongoStore.create);
-
